24825b9d241f1b2f779c56375c599ba98e266d8e
Atique-7 <khanatique0000@gmail.com>
2022-12-23 11:42:05 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let input = document.getElementById("evaluatedText")
+let count = document.getElementById("count")
+
+input.addEventListener("input", () => {
+  count.innerText = input.value.length
+})
